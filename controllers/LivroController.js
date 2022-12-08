07dcf523c72a6568d54eabfd918c7a59e2479502
Smz1n <rogerio.brazilian@gmail.com>
2022-12-08 19:59:43 -0300

    book

--- controllers/LivroController.js
+++ controllers/LivroController.js
@@ -10,7 +10,13 @@ class LivroController {
 
     static findById = (req, res) => {
         const id = req.params.id
-        livros.findById((err, livros) => {
+        livros.findById(id, (err, livros) => {
+           //trata o erro ou o sucesso, baseado na função de callback, utilizando
+            if (err) {
+                res.status(404).send({ message: 'Livro não encontado', err: err.message })
+            } else {
+                res.status(200).json(livros)
+            }
             res.status(200).json(livros)
         })
     }
